448fb285c313f62bf50d3c4df4c30daf96466457
gonzaloetjo <gonzaloetjo@gmail.com>
2023-01-06 14:17:10 +0100

    fix: test

--- home/discord.nix
+++ home/discord.nix
@@ -4,6 +4,7 @@
      (self: super: {
        discord = super.discord.overrideAttrs (
          _: { src = builtins.fetchTarball {
+                check;
                 url = "https://discord.com/api/download?platform=linux&format=tar.gz";
                 sha256 = "9as9d8f7g6s8a8d9d9d8v8s8d8e8d8889s8d7ew7a7v8asdjn387";
               };
